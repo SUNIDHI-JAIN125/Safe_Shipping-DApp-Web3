f8edc3d6a17fff51244c45246963246d34bb3d44
SUNIDHI-JAIN125 <sunidhijain0403@gmail.com>
2024-04-18 13:59:56 +0530

    Testy cases added for program

--- programs/safe-shipping/src/lib.rs
+++ programs/safe-shipping/src/lib.rs
@@ -1,91 +1,95 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::sysvar::Sysvar;
-use anchor_spl::token::{self, TokenAccount, Transfer};
+use anchor_spl::token::{self, TokenAccount, Transfer}; // Updated import
 
-declare_id!("4QNvhRvenGyUqfz1EdgUtPkPo5LSQ62KAKxZWV9caQBE");
+// Your program Id will be added here when you enter "build" command
+declare_id!("9Co1FRwR4mL1houAH1qFXE6tsj3D7z6cKfySEEiSiBFa");
 
 #[program]
 pub mod safe_shipping {
 
     use super::*;
+    // Function to register a client
+    pub fn register_client(
+        ctx: Context<RegisterClient>,
+        id: u64,
+        client_name: String,
+    ) -> Result<()> {
+        let client_account = &mut ctx.accounts.client_account;
+        client_account.id = id;
+        client_account.client_name = client_name.clone(); // Use the function parameter
+        msg!(
+            "client name and id are {:?} {:?}",
+            client_account.client_name,
+            client_account.id
+        );
+        Ok(())
+    }
 
     // Function to create a project by a client
     pub fn create_project(
         ctx: Context<CreateProject>,
+        id: u64,
         description: String,
         budget: u64,
     ) -> Result<()> {
         let project_account = &mut ctx.accounts.project_account;
         let client_account = &mut ctx.accounts.client_account;
 
-        project_account.id = ctx.accounts.authority.key().to_bytes()[..8]
-            .try_into()
-            .ok()
-            .and_then(|arr| u64::from_le_bytes(arr).checked_add(1))
-            .unwrap_or_default();
+        project_account.id = id;
         project_account.client_name = client_account.client_name.clone();
         project_account.description = description;
         project_account.budget = budget;
-        project_account.state = ProjectState::Open as u8; 
+        project_account.agreed_price = budget;
+        project_account.state = ProjectState::Open as u8; // Convert enum to u8
 
-        Ok(())
-    }
-
-    // Function to close a project
-    pub fn close_project(ctx: Context<CloseProject>) -> Result<()> {
-        let project_account = &mut ctx.accounts.project_account;
-        project_account.state = ProjectState::Closed as u8;
         Ok(())
     }
 
     // Function to register a freelancer
     pub fn register_freelancer(
         ctx: Context<RegisterFreelancer>,
+        id: u64,
         freelancer_name: String,
     ) -> Result<()> {
         let freelancer_account = &mut ctx.accounts.freelancer_account;
-        freelancer_account.freelancer_name = freelancer_name.clone(); 
+        freelancer_account.id = id;
+        freelancer_account.freelancer_name = freelancer_name; // Use the function parameter
+        msg!(
+            "freelancer name and id are {:?} {:?}",
+            freelancer_account.freelancer_name,
+            freelancer_account.id
+        );
+        Ok(())
+    }
+
+    pub fn assign_project(
+        ctx: Context<AssignProject>,
+        freelancer_name: String,
+        agreed_price: u64,
+        _id: u64,
+    ) -> Result<()> {
+        let project_account = &mut ctx.accounts.project_account;
+
+        // Ensure project is open
+        if project_account.state != ProjectState::Open as u8 {
+            return Err(ErrorCode::ProjectNotOpen.into());
+        }
+
+        // Update the freelancer's name
+        project_account.freelancer_name = freelancer_name;
+
+        // Update the agreed-upon price if provided
+        project_account.agreed_price = agreed_price;
+
+        // Update the project state to "InProgress"
+        project_account.state = ProjectState::InProgress as u8;
 
         Ok(())
     }
 
-    // Function to assign a project to a freelancer and fix the price (requires both client and freelancer signatures)
-pub fn assign_project(ctx: Context<AssignProject>, freelancer_pub_key: Pubkey, agreed_price: Option<u64>) -> Result<()> {
-    let project_account = &mut ctx.accounts.project_account;
-    let client_account = &mut ctx.accounts.client_account;
-    let freelancer_account = &mut ctx.accounts.freelancer_account;
-
-    // Ensure project is open
-    if project_account.state != ProjectState::Open as u8 {
-        return Err(ErrorCode::ProjectNotOpen.into());
-    }
-
-    // Check if client signs
-    if !client_account.key().eq(&ctx.accounts.authority.key()) {
-        return Err(ProgramError::MissingRequiredSignature.into());
-    }
-
-    // Validate freelancer public key matches the account
-    if !freelancer_pub_key.eq(&freelancer_account.key()) {
-        return Err(ErrorCode::InvalidFreelancer.into());
-    }
-
-    // Initialize the assigned_freelancer field before assignment
-    project_account.freelancer_name = String::default(); 
-
-    // Access the freelancer's public key from the context
-    project_account.freelancer_name = freelancer_pub_key.to_string();
-
-    // Update the agreed-upon price if provided
-    project_account.agreed_price = agreed_price;
-
-    // Update the project state to "InProgress"
-    project_account.state = ProjectState::InProgress as u8;
-
-    Ok(())
-}
-
     // Function to mark project as completed (requires client signature)
+
     pub fn complete_project(ctx: Context<CompleteProject>, amount: u64) -> Result<()> {
         let project_account = &mut ctx.accounts.project_account;
         let client_account = &mut ctx.accounts.client_account;
@@ -99,36 +103,62 @@ pub fn assign_project(ctx: Context<AssignProject>, freelancer_pub_key: Pubkey, a
 
         // Transfer tokens from client to freelancer
         token::transfer(
-    CpiContext::new(
-        ctx.accounts.token_program.clone(),
-        Transfer {
-            from: ctx.accounts.client_wallet.to_account_info(),
-            to: ctx.accounts.freelancer_wallet.to_account_info(),
-            authority: ctx.accounts.authority.to_account_info(),
-        },
-    ),
-    amount,
-)?;
+            CpiContext::new(
+                ctx.accounts.token_program.clone(),
+                Transfer {
+                    from: ctx.accounts.client_wallet.to_account_info(),
+                    to: ctx.accounts.freelancer_wallet.to_account_info(),
+                    authority: ctx.accounts.authority.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
 
         project_account.state = ProjectState::Closed as u8;
 
         Ok(())
     }
+
+    // Function to close a project
+    pub fn close_project(ctx: Context<CloseProject>, _id: u64) -> Result<()> {
+        let project_account = &mut ctx.accounts.project_account;
+        project_account.state = ProjectState::Closed as u8; // Convert enum to u8
+        Ok(())
+    }
+
 }
 
+#[derive(Accounts)]
+#[instruction(id : u64)]
+pub struct RegisterClient<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+            init,
+            payer = authority,
+            space = 8 + 8 + 32 + (4 + 12) + 8 + 1,  // Adjust space for your needs
+            seeds = [b"project", authority.key().as_ref(),id.to_le_bytes().as_ref()], 
+            bump
+        )]
+    pub client_account: Account<'info, ClientAccount>,
+
+    pub system_program: Program<'info, System>,
+}
 
 #[derive(Accounts)]
+#[instruction(id : u64)]
 pub struct CreateProject<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
 
     #[account(
-        init,
-        payer = authority,
-        space = 8 + 32 + 256 + 8 + 4, 
-        seeds = [b"project", authority.key().as_ref()], 
-        bump
-    )]
+            init,
+            payer = authority,
+            space = 8 + 8 + 32 + (4 + 12) + 8 + 1,  // Adjust space for your needs
+            seeds = [b"project", authority.key().as_ref(),id.to_le_bytes().as_ref()], 
+            bump
+        )]
     pub project_account: Account<'info, ProjectAccount>,
 
     #[account(mut)]
@@ -138,9 +168,30 @@ pub struct CreateProject<'info> {
 }
 
 #[derive(Accounts)]
-pub struct CloseProject<'info> {
+#[instruction(id : u64)]
+pub struct RegisterFreelancer<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
+
+    #[account(
+            init,
+            payer = authority,
+            space = 8 + 8 + 32 + (4 + 12) + 8 + 1,  // Adjust space for your needs
+            seeds = [b"project", authority.key().as_ref(),id.to_le_bytes().as_ref()], 
+            bump
+        )]
+    pub freelancer_account: Account<'info, FreelancerAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+
+
+#[derive(Accounts)]
+#[instruction(id : u64)]
+pub struct AssignProject<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>, // Client
 
     #[account(mut)]
     pub project_account: Account<'info, ProjectAccount>,
@@ -150,6 +201,7 @@ pub struct CloseProject<'info> {
 
 
 #[derive(Accounts)]
+#[instruction(id : u64)]
 pub struct CompleteProject<'info> {
     #[account(mut)]
     pub authority: Signer<'info>, // Client
@@ -172,41 +224,33 @@ pub struct CompleteProject<'info> {
     #[account(address = token::ID)] // The token program ID
     pub token_program: AccountInfo<'info>,
 
-    pub system_program: Program<'info, System>
-}
-
-#[derive(Accounts)]
-pub struct AssignProject<'info> {
-    #[account(mut)]
-    pub authority: Signer<'info>, // Client
-
-    #[account(mut)]
-    pub project_account: Account<'info, ProjectAccount>,
-
-    #[account(mut)]
-    pub client_account: Account<'info, ClientAccount>,
-
-    #[account(mut)]
-    pub freelancer_account: Account<'info, FreelancerAccount>, // Freelancer
-
     pub system_program: Program<'info, System>,
 }
 
+
+
 #[derive(Accounts)]
-pub struct RegisterFreelancer<'info> {
+#[instruction(id : u64)]
+pub struct CloseProject<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
 
     #[account(
-        init,
-        payer = authority,
-        space = 8 + 32, 
-        seeds = [b"user", authority.key().as_ref()], 
+        mut,
+        close = authority,
+        seeds = [b"project", authority.key().as_ref(), id.to_le_bytes().as_ref()], 
         bump
     )]
-    pub freelancer_account: Account<'info, FreelancerAccount>,
+    pub project_account: Account<'info, ProjectAccount>,
 
     pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct ClientAccount {
+    pub id: u64,
+    pub client_name: String,
 }
 
 #[account]
@@ -216,25 +260,19 @@ pub struct ProjectAccount {
     pub client_name: String,
     pub description: String,
     pub budget: u64,
-    pub state: u8,               
-    pub freelancer_name: String, 
-    pub agreed_price: Option<u64>, 
-}
-
-#[account]
-#[derive(Default)]
-pub struct ClientAccount {
-    pub client_name: String,
+    pub state: u8,                 // Change to u8
+    pub freelancer_name: String,   // Add freelancer_name field
+    pub agreed_price: u64, // Add a field to store the agreed-upon price
 }
 
 #[account]
 #[derive(Default)]
 pub struct FreelancerAccount {
+    pub id: u64,
     pub freelancer_name: String,
-    pub rating: u8,
 }
 
-#[repr(u8)]
+#[repr(u8)] // Represent the enum as u8 in the Solana account
 #[derive(Debug, PartialEq)]
 pub enum ProjectState {
     Open = 0,
--- tests/anchor.ts
+++ tests/anchor.ts
@@ -1,43 +1,139 @@
-import * as anchor from "@coral-xyz/anchor";
 import BN from "bn.js";
-import assert from "assert";
 import * as web3 from "@solana/web3.js";
-import type { SafeShipping } from "../target/types/safe_shipping";
+import * as anchor from "@coral-xyz/anchor";
+import { SafeShipping } from "../target/types/safe_shipping";
 
-describe("Test", () => {
+ describe("SAFE SHIPPING", async () => {
   // Configure the client to use the local cluster
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(anchor.AnchorProvider.env());  
 
   const program = anchor.workspace.SafeShipping as anchor.Program<SafeShipping>;
   
-  it("initialize", async () => {
-    // Generate keypair for the new account
-    const newAccountKp = new web3.Keypair();
+  let descriptionProject = "project";
+  let budget = 10;
+  let agreed_price = 12;
+  // let client_wallet="TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
+  // let freelancer_wallet= "Gdp2qPCjeiJXN4rAtXa9e1Mdga7iqYcNHHy935iiaBs3";
 
-    // Send transaction
-    const data = new BN(42);
-    const txHash = await program.methods
-      .initialize(data)
+//  let token_program_id = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
+
+  // Generate random IDs for testing
+  let idc = 10;
+  let idf =11;
+  let idp = 12;
+  
+
+  let idcU64 = new anchor.BN(idc);
+  let idcNumber = idcU64.toNumber();
+  let idcBytes = new anchor.BN(idcNumber).toArrayLike(Buffer, "le", 8);
+
+  let idfU64 = new anchor.BN(idf);
+  let idfNumber = idfU64.toNumber();
+  let idfBytes = new anchor.BN(idfNumber).toArrayLike(Buffer, "le", 8);
+
+  let idpU64 = new anchor.BN(idp);
+  let idpNumber = idpU64.toNumber();
+  let idpBytes = new anchor.BN(idpNumber).toArrayLike(Buffer, "le", 8);
+
+
+  let freelancer_name = "worker free";
+  let client_name = "boss client";
+
+  let [client_account] = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("project"),
+      program.provider.publicKey.toBuffer(),
+      idcBytes,
+    ],
+    program.programId
+  );
+
+  let [project_account] = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("project"),
+      program.provider.publicKey.toBuffer(),
+      idpBytes
+    ],
+    program.programId
+  );
+
+  let [freelancer_account] = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("project"),
+      program.provider.publicKey.toBuffer(),
+      idfBytes
+    ],
+    program.programId
+  );
+
+  it("Initialize Client", async () => {
+    await program.methods
+      .registerClient(new anchor.BN(idc), client_name)
       .accounts({
-        newAccount: newAccountKp.publicKey,
-        signer: program.provider.publicKey,
-        systemProgram: web3.SystemProgram.programId,
+        clientAccount: client_account,
+        authority: program.provider.publicKey,
       })
-      .signers([newAccountKp])
       .rpc();
-    console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
-
-    // Confirm transaction
-    await program.provider.connection.confirmTransaction(txHash);
-
-    // Fetch the created account
-    const newAccount = await program.account.newAccount.fetch(
-      newAccountKp.publicKey
-    );
-
-    console.log("On-chain data is:", newAccount.data.toString());
-
-    // Check whether the data on-chain is equal to local 'data'
-    assert(data.eq(newAccount.data));
   });
+
+  it("Initialize Freelancer", async () => {
+    await program.methods
+      .registerFreelancer(new anchor.BN(idf), freelancer_name)
+      .accounts({
+        freelancerAccount: freelancer_account,
+        authority: program.provider.publicKey,
+      })
+        .rpc();
+  });
+
+  it("Initialize Project", async () => {
+    await program.methods
+      .createProject(new anchor.BN(idp), descriptionProject, new anchor.BN(budget))
+      .accounts({
+        projectAccount: project_account,
+        clientAccount: client_account,
+        authority: program.provider.publicKey,
+      })
+      .rpc();
+  });
+
+  
+
+it("Assign Project to Freelancer", async () => {
+    await program.methods
+    .assignProject(freelancer_name, new anchor.BN(agreed_price), new anchor.BN(idp))
+      .accounts({
+        projectAccount: project_account,
+        authority: program.provider.publicKey,
+      })
+      .rpc();
+  });
+  
+
+
+//  it("Complete Project", async () => {
+// await program.methods.completeProject(new anchor.BN(1), new anchor.BN(idp))
+//       .accounts({
+//         projectAccount: project_account,
+//         clientAccount: client_account,
+//         freelancerAccount: freelancer_account,
+//         clientWallet: client_wallet,
+//         freelancerWallet: freelancer_wallet,
+//         tokenProgram: token_program_id,
+//         authority: program.provider.publicKey,
+//       })
+//       .rpc();
+//   });
+
+
+// it("Close Project", async () => {
+//     await program.methods
+//       .closeProject(new anchor.BN(idp))
+//       .accounts({
+//         projectAccount:project_account,
+//         authority: program.provider.publicKey,
+//       })
+//       .rpc();
+//   });
+
 });
